321e3254669a5a5c1358156b5eee010a096c1f28
dbirtola <dbirtola@gmail.com>
2019-06-10 14:01:16 -0600

    Song is in

---
+++ Assets/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private Hv_finalsong_AudioLib pd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        pd = GetComponent<Hv_finalsong_AudioLib>();
+
+        pd.FillTableWithFloatBuffer("strings", new float[] { 1, 40, 45, 50, 55, 59, 64 });
+        pd.FillTableWithFloatBuffer("notes", new float[] { 0, 8, 2, 4, 5, 7, 9, 11, 12});
+
+        pd.FillTableWithFloatBuffer("bass", new float[] { 0, 300, 0, 300, 0, 201, 0 , 203, 0, 203, 0, 300, 0 });
+        pd.FillTableWithFloatBuffer("percussion", new float[] { 0, 300, 0, 300, 0, 201, 0, 203, 0, 203, 0, 300, 0 });
+
+        pd.FillTableWithFloatBuffer("melody", new float[] { 0, 402,501 ,503, 503, 503, 600, 601, 601, 603, 600, 603 });
+
+
+        pd.SendEvent(Hv_finalsong_AudioLib.Event.Bang);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Hv_151Finalfinalfinal_AudioLib pd;
+    public AudioClip _clip;
+    Rigidbody rb;
+
+    float speed = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        pd = GetComponent<Hv_151Finalfinalfinal_AudioLib>();
+        pd.FillTableWithMonoAudioClip("PDTable", _clip);
+
+        pd.SetFloatParameter(Hv_151Finalfinalfinal_AudioLib.Parameter.Frequency, 1);
+        pd.SendEvent(Hv_151Finalfinalfinal_AudioLib.Event.Bang);
+
+        pd.RegisterSendHook();
+        pd.FloatReceivedCallback += OnFloatMessage;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnFloatMessage(Hv_151Finalfinalfinal_AudioLib.FloatMessage message)
+    {
+        Debug.Log(message.receiverName + ": " + message.value);
+      
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown("space"))
+        {
+            pd.SendEvent(Hv_151Finalfinalfinal_AudioLib.Event.Bang);
+        }else if (Input.GetKeyDown("a"))
+        {
+            Debug.Log("a key");
+            pd.SendEvent(Hv_151Finalfinalfinal_AudioLib.Event.Sinenvelope);
+        }
+
+        pd.SetFloatParameter(Hv_151Finalfinalfinal_AudioLib.Parameter.Frequency, moveVertical * (float)0.5);
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+}
